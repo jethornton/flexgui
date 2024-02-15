23ceccb04406a0c5171d44a5f27b1b7d1d46285c
John Thornton <dev@gnipsel.com>
2024-02-15 15:33:10 -0600

    test for all stat labels
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/startup.py
+++ flexgui/src/libflexgui/startup.py
@@ -1,5 +1,7 @@
 import os
 
+from PyQt6.QtWidgets import QLabel
+
 def postgui_hal(parent):
 	postgui_halfiles = parent.inifile.findall("HAL", "POSTGUI_HALFILE") or None
 	if postgui_halfiles is not None:
@@ -9,4 +11,30 @@ def postgui_hal(parent):
 			else:
 				res = os.spawnvp(os.P_WAIT, "halcmd", ["halcmd", "-i", parent.ini_path, "-f", f])
 			if res: raise SystemExit(res)
+
+
+def status_labels(parent):
+	status_items = ['acceleration', 'active_queue', 'actual_position',
+	'adaptive_feed_enabled', 'ain', 'angular_units', 'aout', 'axes', 'axis',
+	'axis_mask', 'block_delete', 'call_level', 'command', 'current_line',
+	'current_vel', 'cycle_time', 'debug', 'delay_left', 'din', 'distance_to_go',
+	'dout', 'dtg', 'echo_serial_number', 'enabled', 'estop', 'exec_state',
+	'feed_hold_enabled', 'feed_override_enabled', 'feedrate', 'file', 'flood',
+	'g5x_index', 'g5x_offset', 'g92_offset', 'gcodes', 'homed', 'id',
+	'ini_filename', 'inpos', 'input_timeout', 'interp_state',
+	'interpreter_errcode', 'joint', 'joint_actual_position', 'joint_position',
+	'joints', 'kinematics_type', 'limit', 'linear_units', 'lube', 'lube_level',
+	'max_acceleration', 'max_velocity', 'mcodes', 'mist', 'motion_line',
+	'motion_mode', 'motion_type', 'optional_stop', 'paused', 'pocket_prepped',
+	'position', 'probe_tripped', 'probe_val', 'probed_position', 'probing',
+	'program_units', 'queue', 'queue_full', 'rapidrate', 'read_line',
+	'rotation_xy', 'settings', 'spindle', 'spindles', 'state', 'task_mode',
+	'task_paused', 'task_state', 'tool_in_spindle', 'tool_from_pocket',
+	'tool_offset', 'tool_table', 'velocity']
+
+	for item in status_items:
+		if parent.findChild(QLabel, f'{item}_lb'):
+			setattr(parent, f'{item}_lb_exists', True)
+		else:
+			setattr(parent, f'{item}_lb_exists', False)
 
