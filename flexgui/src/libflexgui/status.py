994d9ce971597bbd242919a5ee752078e6d0f53c
John Thornton <dev@gnipsel.com>
2024-02-18 13:11:16 -0600

    add joint status
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/status.py
+++ flexgui/src/libflexgui/status.py
@@ -3,7 +3,7 @@ def update(parent):
 	parent.status.poll()
 	for key, value in parent.status_labels.items(): # update all status labels
 		# get the label and set the text to the status value of the key
-		getattr(parent, f'{value}').setText(f'{getattr(parent.status, f"{key}")}')
+		getattr(parent, f'{value}').setText(f'{getattr(parent, "status").key}')
 
 	for key, value in parent.axis_labels.items():
 		if key == 'velocity':
@@ -11,4 +11,9 @@ def update(parent):
 			getattr(parent, f'{value}').setText(f'{vel}')
 		else:
 			getattr(parent, f'{value}').setText(f'{getattr(parent, "status").axis[int(value[5])][key]}')
+
+	for key, value in parent.joint_labels.items():
+		getattr(parent, f'{value}').setText(f'{getattr(parent, "status").joint[int(value[6])][key]}')
+
+
 
