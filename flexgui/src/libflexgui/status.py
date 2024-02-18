c703e9ea5d352d0325a049b6069050ed1e3f70cb
John Thornton <dev@gnipsel.com>
2024-02-18 15:18:37 -0600

    add meaningful status results framework
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/status.py
+++ flexgui/src/libflexgui/status.py
@@ -1,9 +1,21 @@
 
+
+# motion_mode (returns integer) - This is the mode of the Motion controller.
+# One of TRAJ_MODE_COORD, TRAJ_MODE_FREE, TRAJ_MODE_TELEOP
+
+#print(stat_dict['motion_mode'][getattr(parent.status, 'motion_mode')])
+
+
 def update(parent):
 	parent.status.poll()
+	stat_dict = {'motion_mode': {1: 'TRAJ_MODE_FREE', 2: 'TRAJ_MODE_COORD', 3: 'TRAJ_MODE_TELEOP'} }
+
 	for key, value in parent.status_labels.items(): # update all status labels
 		# get the label and set the text to the status value of the key
-		getattr(parent, f'{value}').setText(f'{getattr(parent, "status").key}')
+		if key in stat_dict:
+			getattr(parent, f'{value}').setText(f'{stat_dict[key][getattr(parent.status, f"{key}")]}')
+		else:
+			getattr(parent, f'{value}').setText(f'{getattr(parent.status, f"{key}")}')
 
 	for key, value in parent.axis_labels.items():
 		if key == 'velocity':
