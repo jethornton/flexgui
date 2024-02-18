fdcef6737e7500669b9df0ee59beaeaa3c8fdaaf
John Thornton <dev@gnipsel.com>
2024-02-18 11:48:46 -0600

    round axis vel
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/status.py
+++ flexgui/src/libflexgui/status.py
@@ -6,7 +6,12 @@ def update(parent):
 		getattr(parent, f'{value}').setText(f'{getattr(parent.status, f"{key}")}')
 
 	for key, value in parent.axis_labels.items():
-		getattr(parent, f'{value}').setText(f'{getattr(parent.status, "axis")[int(value[5])][key]}')
+		if key == 'velocity':
+			vel = round(getattr(parent.status, "axis")[int(value[5])][key] * 60)
+			getattr(parent, f'{value}').setText(f'{vel}')
+		else:
+			getattr(parent, f'{value}').setText(f'{getattr(parent.status, "axis")[int(value[5])][key]}')
+		# velocity is units/sec
 
 	# axis_{i}_{item}_lb
 	# axis_0_velocity_lb
