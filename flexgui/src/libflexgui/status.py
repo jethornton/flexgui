73e8783cbef45f0a29ea6a090a542ad293db6021
John Thornton <dev@gnipsel.com>
2024-02-18 11:44:39 -0600

    add axis stats
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/status.py
+++ flexgui/src/libflexgui/status.py
@@ -4,3 +4,9 @@ def update(parent):
 	for key, value in parent.status_labels.items(): # update all status labels
 		# get the label and set the text to the status value of the key
 		getattr(parent, f'{value}').setText(f'{getattr(parent.status, f"{key}")}')
+
+	for key, value in parent.axis_labels.items():
+		getattr(parent, f'{value}').setText(f'{getattr(parent.status, "axis")[int(value[5])][key]}')
+
+	# axis_{i}_{item}_lb
+	# axis_0_velocity_lb
