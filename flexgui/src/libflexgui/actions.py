5c4d2aa4dcd487623e93c88ec8451443be4d666d
John Thornton <dev@gnipsel.com>
2024-02-25 08:35:00 -0600

    work on menu actions
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/actions.py
+++ flexgui/src/libflexgui/actions.py
@@ -12,16 +12,25 @@ def action_edit(parent): # actionEdit
 def action_reload(parent): # actionReload
 	print(parent.sender().objectName())
 
-def action_edit_tools(parent): # actionEdit_Tools
+def action_save_as(parent): # actionSave_As
 	print(parent.sender().objectName())
 
-def action_reload_tools(parent): # actionReload_Tools
+def action_edit_tool_table(parent): # actionEdit_Tool_Table
+	print(parent.sender().objectName())
+
+def action_reload_tool_table(parent): # actionReload_Tool_Table
+	print(parent.sender().objectName())
+
+def action_ladder_editor(parent): # actionLadder_Editor
 	print(parent.sender().objectName())
 
 def action_quit(parent): # actionQuit
 	print(parent.sender().objectName())
 
 def action_clear_mdi(parent): # actionClear_MDI
+	print(parent.sender().objectName())
+
+def action_copy_mdi(parent): # actionCopy_MDI
 	print(parent.sender().objectName())
 
 def action_show_hal(parent): # actionShow_HAL
@@ -33,6 +42,12 @@ def action_hal_meter(parent): # actionHal_Meter
 	print(parent.sender().objectName())
 
 def action_hal_scope(parent): # actionHal_Scope
+	print(parent.sender().objectName())
+
+def action_about(parent): # actionAbout
+	print(parent.sender().objectName())
+
+def action_quick_reference(parent): # actionQuick_Reference
 	print(parent.sender().objectName())
 
 
