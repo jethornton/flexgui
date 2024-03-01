b792d9127cbfdbdf51ef75181f85b95b64a1c4a2
John Thornton <dev@gnipsel.com>
2024-03-01 06:30:23 -0600

    menu items are handled in actions
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/connections.py
+++ flexgui/src/libflexgui/connections.py
@@ -4,7 +4,5 @@ from PyQt6.QtWidgets import QPushButton, QPlainTextEdit, QListWidget, QSlider
 from PyQt6.QtWidgets import QLineEdit
 from PyQt6.QtGui import QAction
 
-
-from libflexgui import menus
 from libflexgui import editor
 from libflexgui import utilities
