70b087f17b94711b7f27bbf0f87dee97a3326ebb
John Thornton <dev@gnipsel.com>
2024-03-05 15:21:14 -0600

    finish up recent files menu
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/actions.py
+++ flexgui/src/libflexgui/actions.py
@@ -1,5 +1,7 @@
 import os
 
+from functools import partial
+
 from PyQt5.QtWidgets import QApplication, QFileDialog
 
 app = QApplication([])
@@ -12,6 +14,39 @@ def load_file(parent, gcode_file):
 	#parent.actionReload.setEnabled(True)
 	base = os.path.basename(gcode_file)
 
+	keys = parent.settings.allKeys()
+	files = []
+	for key in keys:
+		if key.startswith('recent_files'):
+			files.append(parent.settings.value(key))
+			#print(parent.settings.value(key))
+	if gcode_file in files:
+		files.remove(gcode_file)
+
+	files.insert(0, gcode_file)
+	#print(files)
+
+	files = files[:5]
+	#print(files)
+
+	parent.settings.beginGroup('recent_files')
+	parent.settings.remove('')
+	for i, item in enumerate(files):
+		parent.settings.setValue(str(i), item)
+	parent.settings.endGroup()
+
+	# clear the recent menu
+	parent.menuRecent.clear()
+
+	# add the recent files from settings
+	keys = parent.settings.allKeys()
+	for key in keys:
+		if key.startswith('recent_files'):
+			path = parent.settings.value(key)
+			name = os.path.basename(path)
+			a = parent.menuRecent.addAction(name)
+			a.triggered.connect(partial(load_file, parent, path))
+
 def action_open(parent): # actionOpen
 	if os.path.isdir(os.path.expanduser('~/linuxcnc/nc_files')):
 		gcode_dir = os.path.expanduser('~/linuxcnc/nc_files')
@@ -22,23 +57,6 @@ def action_open(parent): # actionOpen
 	filter='G code Files (*.ngc *.NGC);;All Files (*)', options=QFileDialog.DontUseNativeDialog,)
 	if gcode_file: load_file(parent, gcode_file)
 
-	'''
-		keys = parent.settings.allKeys()
-		files = []
-		for key in keys:
-			if key.startswith('recent_files'):
-				files.append(parent.settings.value(key))
-				#print(parent.settings.value(key))
-		files.insert(0, gcode_file)
-		#print(files)
-		files = files[:5]
-		#print(files)
-		parent.settings.beginGroup('recent_files')
-		parent.settings.remove('')
-		for i, item in enumerate(files):
-			parent.settings.setValue(str(i), item)
-		parent.settings.endGroup()
-	'''
 def action_test(parent, text):
 	print(text)
 	print(parent.sender().objectName())
