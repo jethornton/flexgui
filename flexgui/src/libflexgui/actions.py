7417dc7e7c42400770f109c8bc3c7a267336f3b0
John Thornton <dev@gnipsel.com>
2024-03-11 09:05:47 -0500

    enable run after loading a file
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/actions.py
+++ flexgui/src/libflexgui/actions.py
@@ -14,6 +14,9 @@ app = QApplication([])
 def load_file(parent, gcode_file):
 	parent.command.program_open(gcode_file)
 	parent.command.wait_complete()
+	for item in parent.file_enable:
+		getattr(parent, item).setEnabled(True)
+
 	text = open(gcode_file).read()
 	if parent.gcode_pte_exists:
 		parent.gcode_pte.setPlainText(text)
