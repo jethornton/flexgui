a4c70051eabf274c75e2b3b1878d297f024a8c01
John Thornton <dev@gnipsel.com>
2024-03-13 07:48:04 -0500

    work on home/unhome enables
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/utilities.py
+++ flexgui/src/libflexgui/utilities.py
@@ -12,3 +12,14 @@ def all_homed(parent):
 		elif h == 1: all_homed = True
 	return all_homed
 
+def all_unhomed(parent):
+	parent.status.poll()
+	num_joints = parent.status.joints
+	for i,h in enumerate(parent.status.homed):
+		if i >= num_joints: break
+		if h == 1:
+			all_unhomed = False
+			break
+		elif h == 0: all_unhomed = True
+	return all_unhomed
+
