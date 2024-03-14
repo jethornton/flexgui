558ca2a90d068d90f0830dabc9b778803c74c26b
John Thornton <dev@gnipsel.com>
2024-03-14 07:34:07 -0500

    fix space
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/utilities.py
+++ flexgui/src/libflexgui/utilities.py
@@ -4,7 +4,7 @@
 def all_homed(parent):
 	parent.status.poll()
 	num_joints = parent.status.joints
-	for i,h in enumerate(parent.status.homed):
+	for i, h in enumerate(parent.status.homed):
 		if i >= num_joints: break
 		if h == 0:
 			all_homed = False
