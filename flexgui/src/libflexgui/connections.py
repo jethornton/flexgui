4909ddc5def9cfe76e40638e4e5796431c8639a2
John Thornton <dev@gnipsel.com>
2024-05-11 07:49:15 -0500

    delete unused file
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/connections.py
+++
@@ -1,8 +0,0 @@
-from functools import partial
-
-from PyQt6.QtWidgets import QPushButton, QPlainTextEdit, QListWidget, QSlider
-from PyQt6.QtWidgets import QLineEdit
-from PyQt6.QtGui import QAction
-
-from libflexgui import editor
-from libflexgui import utilities
