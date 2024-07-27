801f61e3c4594e36718fba43b00f105281f9ead5
John Thornton <dev@gnipsel.com>
2024-07-27 07:00:34 -0500

    add plot widget
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/startup.py
+++ flexgui/src/libflexgui/startup.py
@@ -998,7 +998,7 @@ def setup_hal_buttons(parent):
 def setup_plot(parent):
 	if 'plot_widget' in parent.children:
 		from libflexgui import flexplot
-		plotter = flexplot.graphics(parent)
+		plotter = flexplot.emc_plot(parent)
 		layout = QVBoxLayout(parent.plot_widget)
 		layout.addWidget(plotter)
 
