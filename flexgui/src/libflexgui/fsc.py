d7fa155a04895d9adc2a12ded10f894ca4e4dc55
John Thornton <dev@gnipsel.com>
2024-08-14 07:57:04 -0500

    add error checking to fsc
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/fsc.py
+++ flexgui/src/libflexgui/fsc.py
@@ -4,8 +4,11 @@ from math import pi
 from PyQt6.QtWidgets import QWidget
 from PyQt6.uic import loadUi
 
+from libflexgui import utilities
+from libflexgui import dialogs
+
 class feed_speed_calc(QWidget):
-	def __init__(self):
+	def __init__(self, touch=False):
 		super().__init__()
 		self.path = os.path.dirname(os.path.realpath(sys.argv[0]))
 		if self.path == '/usr/bin':
@@ -22,10 +25,77 @@ class feed_speed_calc(QWidget):
 		self.fsc_calc_sfm_pb.clicked.connect(self.calc_sfm)
 		self.fsc_units_pb.clicked.connect(self.units)
 
+	def check_cl(self):
+		if self.fsc_chip_load_le.text() == '':
+			msg = ('Chip Load can not be blank')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+		if utilities.is_float(self.fsc_chip_load_le.text()):
+			return float(self.fsc_chip_load_le.text())
+		else:
+			msg = ('Chip Load is not a valid number')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+
+	def check_feed(self):
+		if self.fsc_feed_le.text() == '':
+			msg = ('Feed can not be blank')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+		if utilities.is_float(self.fsc_feed_le.text()):
+			return float(self.fsc_feed_le.text())
+		else:
+			msg = ('Feed is not a valid number')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+
+	def check_rpm(self):
+		if self.fsc_rpm_le.text() == '':
+			msg = ('RPM can not be blank')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+		if utilities.is_float(self.fsc_rpm_le.text()):
+			return float(self.fsc_rpm_le.text())
+		else:
+			msg = ('RPM is not a valid number')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+
+	def check_flutes(self):
+		if self.fsc_flutes_le.text() == '':
+			msg = ('Flutes can not be blank')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+		if utilities.is_int(self.fsc_flutes_le.text()):
+			return int(self.fsc_flutes_le.text())
+		else:
+			msg = ('Flutes is not a valid number')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+
+	def check_dia(self):
+		if self.fsc_diameter_le.text() == '':
+			msg = ('Diameter can not be blank')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+		if utilities.is_float(self.fsc_diameter_le.text()):
+			return float(self.fsc_diameter_le.text())
+		else:
+			msg = ('Flutes is not a valid number')
+			dialogs.warn_msg_ok(msg, 'Error')
+			return False
+
 	def calc_cl(self):
-		feed = float(self.fsc_feed_le.text())
-		rpm = float(self.fsc_rpm_le.text())
-		flutes = int(self.fsc_flutes_le.text())
+		feed = self.check_feed()
+		if not feed:
+			return
+		rpm = self.check_rpm()
+		if not rpm:
+			return
+		flutes = self.check_flutes()
+		if not flutes:
+			return
+
 		if self.fsc_units_pb.text() == 'Inch':
 			# Chip Load = inches per minute / (RPM x number of flutes)
 			cl = feed / (rpm * flutes)
@@ -36,9 +106,16 @@ class feed_speed_calc(QWidget):
 			self.fsc_cl_lb.setText(f'{cl:.3f} mm/PT')
 
 	def calc_fr(self):
-		cl = float(self.fsc_chip_load_le.text())
-		rpm = float(self.fsc_rpm_le.text())
-		flutes = int(self.fsc_flutes_le.text())
+		cl = self.check_cl()
+		if not cl:
+			return
+		rpm = self.check_rpm()
+		if not rpm:
+			return
+		flutes = self.check_flutes()
+		if not flutes:
+			return
+
 		if self.fsc_units_pb.text() == 'Inch':
 			feed = cl * (rpm * flutes)
 			self.fsc_feed_lb.setText(f'{feed:.2f} IPM')
@@ -48,8 +125,13 @@ class feed_speed_calc(QWidget):
 			self.fsc_feed_lb.setText(f'{feed:.2f} MPM')
 
 	def calc_sfm(self):
-		dia = float(self.fsc_diameter_le.text())
-		rpm = float(self.fsc_rpm_le.text())
+		dia = self.check_dia()
+		if not dia:
+			return
+		rpm = self.check_rpm()
+		if not rpm:
+			return
+
 		if self.fsc_units_pb.text() == 'Inch':
 			sfm = (rpm * dia)/3.82
 			self.fsc_sfm_lb.setText(f'{sfm:.2f} SFM')
