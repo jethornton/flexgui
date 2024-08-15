12d84d7de7eb7d7fba51f7fb5b18b534bd5ff7a8
John Thornton <dev@gnipsel.com>
2024-08-15 11:06:19 -0500

    work on dsf
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/dsf.py
+++ flexgui/src/libflexgui/dsf.py
@@ -17,6 +17,8 @@ from PyQt6.uic import loadUi
 • S.F.M. = 0.2618 x Drill Diameter x R.P.M.
 • I.P.M. = I.P.R. (feed) x R.P.M. (speed)
 • Machine Time (seconds) = (60 x Feed minus Stroke) / I.P.M.
+1 inch is exactly 25.4 MM Therefore 1 meter is about 39.37 IN, or 3.281 FT.
+1 foot is 0.3048 meters
 '''
 
 class dsf_calc(QWidget):
@@ -29,19 +31,24 @@ class dsf_calc(QWidget):
 			self.lib_path = os.path.join(self.path, 'libflexgui')
 		loadUi(os.path.join(self.lib_path, 'dsf.ui'), self)
 
-		self.dsf_units_pb.clicked.connect(self.units)
+		self.units = 'Inch'
+		self.dsf_units_pb.clicked.connect(self.change_units)
 		self.setup_material()
 
 
 
-	def units(self):
-		if self.dsf_units_pb.isChecked():
+	def change_units(self):
+		if self.units == 'Inch':
 			self.dsf_units_pb.setText('Metric')
-		else:
+			self.units = 'Metric'
+		elif self.units == 'Metric':
 			self.dsf_units_pb.setText('Inch')
+			self.units = 'Inch'
+		self.setup_material()
 
 
 	def setup_material(self):
+		self.dsf_material_cb.clear()
 		self.dsf_material_cb.addItem('Select')
 		sfm = [
 			'Aluminum: 200-300 SFM',
@@ -54,8 +61,25 @@ class dsf_calc(QWidget):
 			'Tool Steel 1.2 carbon:  50-60 SFM',
 			'',
 		]
+		smm = [
+			'Aluminum: 60-90 SMM',
+			'Brass and Bronze: 45-90 SMM',
+			'Bronze (High Tensile) 21-45 SMM',
+			'Iron-Cast (Soft): 23-38 SMM',
+			'Plastics: 30-90 SMM',
+			'Steel .2 to .3 carbon:  25-34 SMM',
+			'Steel .4 to .5 carbon:  21-25 SMM',
+			'Tool Steel 1.2 carbon:  15-18 SMM',
+			'',
+		]
 
-		self.dsf_material_cb.addItems(sfm)
+		if self.units == 'Inch':
+			self.dsf_material_cb.addItems(sfm)
+		else:
+			self.dsf_material_cb.addItems(smm)
+
+
+
 
 
 
