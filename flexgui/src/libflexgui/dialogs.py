e216fc986829d2117259286a6aae9536dcda5679
John Thornton <dev@gnipsel.com>
2024-09-21 06:21:15 -0500

    add critical message and keyboard popup
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/dialogs.py
+++ flexgui/src/libflexgui/dialogs.py
@@ -26,6 +26,37 @@ def gcode(self, obj):
 	if result:
 		obj.setText(gp.retval())
 
+def keyboard(self, obj):
+	kb = keyboard_pad.keyboard_pad()
+	result = gp.exec()
+	if result:
+		obj.setText(gp.retval())
+
+def question_msg_yes_no_check(body_text, chkbx_text, title=None):
+	chkBox = QCheckBox()
+	chkBox.setText(chkbx_text)
+	msg_box = QMessageBox()
+	msg_box.setCheckBox(chkBox)
+	msg_box.setIcon(QMessageBox.Icon.Question)
+	msg_box.setWindowTitle(title)
+	msg_box.setText(body_text)
+	msg_box.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+	returnValue = msg_box.exec()
+	answer = True if returnValue == QMessageBox.StandardButton.Yes else False
+	return answer, chkBox.isChecked()
+
+def info_msg_ok(text, title=None):
+	msg_box = QMessageBox()
+	msg_box.setIcon(QMessageBox.Icon.Information)
+	msg_box.setWindowTitle(title)
+	msg_box.setText(text)
+	msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+	returnValue = msg_box.exec()
+	if returnValue == QMessageBox.StandardButton.Ok:
+		return True
+	else:
+		return False
+
 def warn_msg_ok(text, title=None):
 	msg_box = QMessageBox()
 	msg_box.setIcon(QMessageBox.Icon.Warning)
@@ -75,22 +106,9 @@ def warn_msg_yes_no_check(body_text, chkbx_text, title=None):
 	answer = True if returnValue == QMessageBox.StandardButton.Yes else False
 	return answer, chkBox.isChecked()
 
-def question_msg_yes_no_check(body_text, chkbx_text, title=None):
-	chkBox = QCheckBox()
-	chkBox.setText(chkbx_text)
+def critical_msg_ok(text, title=None):
 	msg_box = QMessageBox()
-	msg_box.setCheckBox(chkBox)
-	msg_box.setIcon(QMessageBox.Icon.Question)
-	msg_box.setWindowTitle(title)
-	msg_box.setText(body_text)
-	msg_box.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
-	returnValue = msg_box.exec()
-	answer = True if returnValue == QMessageBox.StandardButton.Yes else False
-	return answer, chkBox.isChecked()
-
-def info_msg_ok(text, title=None):
-	msg_box = QMessageBox()
-	msg_box.setIcon(QMessageBox.Icon.Information)
+	msg_box.setIcon(QMessageBox.Icon.Critical)
 	msg_box.setWindowTitle(title)
 	msg_box.setText(text)
 	msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
