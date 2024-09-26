df4b01b9b49ddce62c7726c4636d514b07bb4d8a
John Thornton <dev@gnipsel.com>
2024-09-26 06:39:03 -0500

    disable run controls when probing is enabled
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/probe.py
+++ flexgui/src/libflexgui/probe.py
@@ -9,7 +9,13 @@ def toggle(parent):
 			parent.spindle_speed_lb.setText(f'{parent.spindle_speed}')
 		parent.command.spindle(emc.SPINDLE_OFF)
 
+		for key, value in parent.program_running.items():
+			getattr(parent, key).setEnabled(False)
+
 	else:
 		for item in parent.probe_controls:
 			getattr(parent, item).setEnabled(False)
+
+		for key, value in parent.program_running.items():
+			getattr(parent, key).setEnabled(True)
 
