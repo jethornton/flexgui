c09ee1773b4583d9e6254338c228d464fb482ebc
John Thornton <dev@gnipsel.com>
2024-09-27 13:00:51 -0500

    fix run controls enable disable while probing is enabled
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/probe.py
+++ flexgui/src/libflexgui/probe.py
@@ -2,6 +2,7 @@ import linuxcnc as emc
 
 def toggle(parent):
 	if parent.sender().isChecked():
+		parent.probing = True
 		for item in parent.probe_controls:
 			getattr(parent, item).setEnabled(True)
 		parent.spindle_speed = 0
@@ -13,6 +14,7 @@ def toggle(parent):
 			getattr(parent, key).setEnabled(False)
 
 	else:
+		parent.probing = False
 		for item in parent.probe_controls:
 			getattr(parent, item).setEnabled(False)
 
