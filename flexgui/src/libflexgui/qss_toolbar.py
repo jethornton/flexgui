2ac8cdb54a02e42e3eb2f7816bb21df5521da62a
John Thornton <dev@gnipsel.com>
2024-12-28 07:34:38 -0600

    remove test code
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/qss_toolbar.py
+++ flexgui/src/libflexgui/qss_toolbar.py
@@ -6,83 +6,6 @@ def startup(parent):
 
 	parent.tbar_normal = False
 
-	tb_style = 'QToolBar {\n'
-	tb_style += 'background: rgb(85, 170, 125);\n'
-	tb_style += 'spacing: 30px;\n'
-	tb_style += 'border-style: solid;\n'
-	tb_style += 'border-color: #000000;\n'
-	tb_style += 'border-width: 4px;\n'
-	tb_style += 'border-radius: 10px;\n'
-	tb_style += 'padding: 5px;\n'
-	tb_style += '}\n'
-
-	'''
-	tb_style += 'QToolButton {\n'
-	tb_style += 'border: 2px solid #8f8f91;\n'
-	tb_style += 'border-radius: 6px;\n'
-	tb_style += 'background-color: #c0c0c0;\n'
-	tb_style += '}\n'
-	tb_style += 'QToolButton:hover {\n'
-	tb_style += 'border: 2px solid purple;\n'
-	tb_style += 'border-radius: 6px;\n'
-	tb_style += 'background-color: green\n'
-	tb_style += '}'
-	tb_style += 'QToolButton:pressed {\n'
-	tb_style += 'border: 2px solid #8f8f91;\n'
-	tb_style += 'border-radius: 6px;\n'
-	tb_style += 'background-color: yellow\n'
-	tb_style += '}'
-	tb_style += 'QToolButton:checked {\n'
-	tb_style += 'color: 2px solid yellow;\n'
-	tb_style += 'border: 2px solid red;\n'
-	tb_style += 'border-radius: 6px;\n'
-	tb_style += 'background-color: green\n'
-	tb_style += '}'
-	'''
-	'''
-
-  background-color:
-	 /* all types of tool button */
-			border: 2px solid #8f8f91;
-			border-radius: 6px;
-			background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-			                                  stop: 0 #f6f7fa, stop: 1 #dadbde);
-	}
-	tb_style += 'QToolButton {\n'
-	tb_style += 'background-color: #e0e0e0;\n'
-	tb_style += 'border: 1px solid #c0c0c0;\n'
-	tb_style += 'padding: 5px;\n'
-	tb_style += 'margin: 2px;\n'
-	tb_style += 'QToolButton:hover {\n'
-	tb_style += 'background-color: #d0d0d0;\n'
-	tb_style += '}\n'
-	tb_style += 'QToolButton:pressed {\n'
-	tb_style += 'background-color: #c0c0c0;\n'
-	tb_style += '}'
-	'''
-	#parent.toolBar.setStyleSheet('''
-	#QToolBar {
-	#background: red;
-	#spacing: 3px;
-	#}
-	#''')
-	parent.toolBar.setStyleSheet(tb_style)
-	#parent.toolBar.setStyleSheet(textwrap.dedent(tb_style))
-
-	'''
-	def hex_to_rgb(hex_color):
-			"""Converts a hex color code to RGB."""
-
-			hex_color = hex_color.lstrip('#')
-			return tuple(int(hex_color[i:i + 2], 16) for i in (0, 2, 4))
-
-	hex_color = "#e0e0e0"
-	rgb_color = hex_to_rgb(hex_color)
-
-	print(rgb_color)  # Output: (224, 224, 224)
-	'''
-
-
 	# QToolBar
 	parent.tbar_bg_color_normal.clicked.connect(parent.color_dialog)
 	parent.toolbar_spacing.valueChanged.connect(partial(spacing, parent))
@@ -109,7 +32,6 @@ def startup(parent):
 	parent.tbar_margin_right_normal.valueChanged.connect(parent.margin)
 	parent.tbar_margin_top_normal.valueChanged.connect(parent.margin)
 	parent.tbar_margin_bottom_normal.valueChanged.connect(parent.margin)
-
 
 ######### QToolBar Stylesheet #########
 
@@ -161,7 +83,6 @@ def tbar_create_stylesheet(parent):
 		if parent.tbar_margin_bottom_normal.value() > 0:
 			style += f'\tmargin-bottom: {parent.tbar_margin_bottom_normal.value()};\n'
 
-
 		style += '}\n' # End of QToolBar
 
 	parent.tbar_stylesheet.clear()
@@ -176,8 +97,5 @@ def spacing(parent):
 	obj = parent.sender().objectName().split('_')[0]
 	if parent.sender().value() > 0:
 		parent.tbar_normal = True
-
-
-
 
 
