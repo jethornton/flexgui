50aa8a62919c30202c6ace0890dcecbda94ff5f5
John Thornton <dev@gnipsel.com>
2024-12-28 17:50:12 -0600

    work on toolbutton
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/qss_toolbutton.py
+++ flexgui/src/libflexgui/qss_toolbutton.py
@@ -8,13 +8,33 @@ def startup(parent):
 	parent.tb_normal = False
 	parent.tb_apply_style.clicked.connect(partial(tb_create_stylesheet, parent))
 
-	parent.tb_fg_color_normal.clicked.connect(parent.color_dialog)
-	parent.tb_bg_color_normal.clicked.connect(parent.color_dialog)
+	parent.tb_min_width_normal.valueChanged.connect(parent.size)
+	parent.tb_min_height_normal.valueChanged.connect(parent.size)
+	parent.tb_max_width_normal.valueChanged.connect(parent.size)
+	parent.tb_max_height_normal.valueChanged.connect(parent.size)
 
+	border_types = ['none', 'solid', 'dashed', 'dotted', 'double', 'groove',
+		'ridge', 'inset', 'outset']
+	abstract_button_states = ['normal', 'hover', 'pressed', 'checked', 'disabled']
 
-	parent.tbtn_fg_color_normal = False
-	parent.tbtn_bg_color_normal = False
+	for item in abstract_button_states: # populate border combo boxes
+		getattr(parent, f'tb_border_type_{item}').addItems(border_types)
+		setattr(parent, f'tb_{item}', False)
+		setattr(parent, f'tbtn_fg_color_{item}', False)
+		setattr(parent, f'tbtn_bg_color_{item}', False)
+		setattr(parent, f'tbtn_border_color_{item}', False)
 
+	for state in abstract_button_states: # color dialog connections
+		getattr(parent, f'tb_fg_color_{state}').clicked.connect(parent.color_dialog)
+		getattr(parent, f'tb_bg_color_{state}').clicked.connect(parent.color_dialog)
+		getattr(parent, f'tb_border_color_{state}').clicked.connect(parent.color_dialog)
+
+	parent.tb_font_picker.clicked.connect(parent.font_dialog)
+	parent.tb_font_family = False
+	parent.tb_font_size = False
+	parent.tb_font_weight = False
+	parent.tb_font_style = False
+	parent.tb_font_italic = False
 
 ######### QToolButton Stylesheet #########
 
@@ -39,6 +59,36 @@ def tb_create_stylesheet(parent):
 			style += f'\tcolor: {parent.tbtn_fg_color_normal};\n'
 		if parent.tbtn_bg_color_normal:
 			style += f'\tbackground-color: {parent.tbtn_bg_color_normal};\n'
+
+		# size
+		if parent.tb_min_width_normal.value() > 0:
+			style += f'\tmin-width: {parent.tb_min_width_normal.value()};\n'
+		if parent.tb_min_height_normal.value() > 0:
+			style += f'\tmin-height: {parent.tb_min_height_normal.value()};\n'
+		if parent.tb_max_width_normal.value() > 0:
+			style += f'\tmax-width: {parent.tb_max_width_normal.value()};\n'
+		if parent.tb_max_height_normal.value() > 0:
+			style += f'\tmax-height: {parent.tb_max_height_normal.value()};\n'
+
+		# border
+		border_type_normal = parent.tb_border_type_normal.currentText()
+		if border_type_normal != 'none':
+			style += f'\tborder-style: {border_type_normal};\n'
+		if parent.tbtn_border_color_normal:
+			style += f'\tborder-color: {parent.tbtn_border_color_normal};\n'
+		if parent.tb_border_width_normal.value() > 0:
+			style += f'\tborder-width: {parent.tb_border_width_normal.value()}px;\n'
+		if parent.tb_border_radius_normal.value() > 0:
+			style += f'\tborder-radius: {parent.tb_border_radius_normal.value()}px;\n'
+
+		# font
+		if parent.tb_font_family:
+			style += f'\tfont-family: {parent.tb_font_family};\n'
+		if parent.tb_font_size:
+			style += f'\tfont-size: {parent.tb_font_size}pt;\n'
+		if parent.tb_font_weight:
+			style += f'\tfont-weight: {parent.tb_font_weight};\n'
+
 
 		style += '\tspacing: 25px;\n'
 
