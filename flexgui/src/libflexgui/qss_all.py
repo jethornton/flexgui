3a2fef3096a40113981eb1bf6c87938d02e73377
John Thornton <dev@gnipsel.com>
2025-01-02 07:08:41 -0600

    only change colors that are not set
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/qss_all.py
+++ flexgui/src/libflexgui/qss_all.py
@@ -20,6 +20,18 @@ def startup(parent):
 	parent.all_fg_color_sel_normal = False
 	parent.all_bg_color_sel_normal = False
 
+	parent.all_fg_color_hover.clicked.connect(parent.color_dialog)
+	parent.all_bg_color_hover.clicked.connect(parent.color_dialog)
+
+	parent.all_fg_color_sel_hover = False
+	parent.all_bg_color_sel_hover = False
+
+	parent.all_fg_color_checked.clicked.connect(parent.color_dialog)
+	parent.all_bg_color_checked.clicked.connect(parent.color_dialog)
+
+	parent.all_fg_color_sel_checked = False
+	parent.all_bg_color_sel_checked = False
+
 	parent.all_fg_color_disabled.clicked.connect(parent.color_dialog)
 	parent.all_bg_color_disabled.clicked.connect(parent.color_dialog)
 
@@ -34,160 +46,341 @@ def all_create_stylesheet(parent):
 		color = parent.all_fg_color_sel_normal
 
 		# QPushButton
-		parent.pb_normal = True
-		label = parent.pb_fg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.pb_fg_color_sel_normal = color
+		if not parent.pb_fg_color_sel_normal:
+			parent.pb_normal = True
+			label = parent.pb_fg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_fg_color_sel_normal = color
 
 		# QCheckBox
-		parent.cb_normal = True
-		label = parent.cb_fg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.cb_fg_color_sel_normal = color
+		if not parent.cb_fg_color_sel_normal:
+			parent.cb_normal = True
+			label = parent.cb_fg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_fg_color_sel_normal = color
 
 		# QRadioButton
-		parent.rb_normal = True
-		label = parent.rb_fg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.rb_fg_color_sel_normal = color
+		if not parent.rb_fg_color_sel_normal:
+			parent.rb_normal = True
+			label = parent.rb_fg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_fg_color_sel_normal = color
 
 		# QToolButton
-		parent.tb_normal = True
-		label = parent.tb_fg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.tb_fg_color_sel_normal = color
+		if not parent.tb_fg_color_sel_normal:
+			parent.tb_normal = True
+			label = parent.tb_fg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_fg_color_sel_normal = color
 
 		# QSpinBox
-		parent.sb_normal = True
-		label = parent.sb_fg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.sb_fg_color_sel_normal = color
+		if not parent.sb_fg_color_sel_normal:
+			parent.sb_normal = True
+			label = parent.sb_fg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.sb_fg_color_sel_normal = color
 
 		# QLabel
-		parent.lb_normal = True
-		label = parent.lb_fg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.lb_fg_color_sel_normal = color
+		if not parent.lb_fg_color_sel_normal:
+			parent.lb_normal = True
+			label = parent.lb_fg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.lb_fg_color_sel_normal = color
 
 	# Background Color Normal
 	if parent.all_bg_color_sel_normal:
 		color = parent.all_bg_color_sel_normal
 
 		# QPushButton
-		parent.pb_normal = True
-		label = parent.pb_bg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.pb_bg_color_sel_normal = color
+		if not parent.pb_bg_color_sel_normal:
+			parent.pb_normal = True
+			label = parent.pb_bg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_bg_color_sel_normal = color
 
 		# QCheckBox
-		parent.cb_normal = True
-		label = parent.cb_bg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.cb_bg_color_sel_normal = color
+		if not parent.cb_bg_color_sel_normal:
+			parent.cb_normal = True
+			label = parent.cb_bg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_bg_color_sel_normal = color
 
 		# QRadioButton
-		parent.rb_normal = True
-		label = parent.rb_bg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.rb_bg_color_sel_normal = color
+		if not parent.rb_bg_color_sel_normal:
+			parent.rb_normal = True
+			label = parent.rb_bg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_bg_color_sel_normal = color
 
 		# QToolButton
-		parent.tb_normal = True
-		label = parent.tb_bg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.tb_bg_color_sel_normal = color
+		if not parent.tb_bg_color_sel_normal:
+			parent.tb_normal = True
+			label = parent.tb_bg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_bg_color_sel_normal = color
 
 		# QSpinBox
-		parent.sb_normal = True
-		label = parent.sb_bg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.sb_bg_color_sel_normal = color
+		if not parent.sb_bg_color_sel_normal:
+			parent.sb_normal = True
+			label = parent.sb_bg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.sb_bg_color_sel_normal = color
 
 		# QLabel
-		parent.lb_normal = True
-		label = parent.lb_bg_color_normal.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.lb_bg_color_sel_normal = color
+		if not parent.lb_bg_color_sel_normal:
+			parent.lb_normal = True
+			label = parent.lb_bg_color_normal.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.lb_bg_color_sel_normal = color
+
+	# Foreground Color Hover
+	if parent.all_fg_color_sel_hover:
+		color = parent.all_fg_color_sel_hover
+
+		# QPushButton
+		if not parent.pb_fg_color_sel_hover:
+			parent.pb_hover = True
+			label = parent.pb_fg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_fg_color_sel_hover = color
+
+		# QCheckBox
+		if not parent.cb_fg_color_sel_hover:
+			parent.cb_hover = True
+			label = parent.cb_fg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_fg_color_sel_hover = color
+
+		# QRadioButton
+		if not parent.rb_fg_color_sel_hover:
+			parent.rb_hover = True
+			label = parent.rb_fg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_fg_color_sel_hover = color
+
+		# QToolButton
+		if not parent.tb_fg_color_sel_hover:
+			parent.tb_hover = True
+			label = parent.tb_fg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_fg_color_sel_hover = color
+
+		# QSpinBox
+		if not parent.sb_fg_color_sel_hover:
+			parent.sb_hover = True
+			label = parent.sb_fg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.sb_fg_color_sel_hover = color
+
+		# QLabel
+		if not parent.lb_fg_color_sel_hover:
+			parent.lb_hover = True
+			label = parent.lb_fg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.lb_fg_color_sel_hover = color
+
+	# Background Color Hover
+	if parent.all_bg_color_sel_hover:
+		color = parent.all_bg_color_sel_hover
+
+		# QPushButton
+		if not parent.pb_bg_color_sel_hover:
+			parent.pb_hover = True
+			label = parent.pb_bg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_bg_color_sel_hover = color
+
+		# QCheckBox
+		if not parent.cb_bg_color_sel_hover:
+			parent.cb_hover = True
+			label = parent.cb_bg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_bg_color_sel_hover = color
+
+		# QRadioButton
+		if not parent.rb_bg_color_sel_hover:
+			parent.rb_hover = True
+			label = parent.rb_bg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_bg_color_sel_hover = color
+
+		# QToolButton
+		if not parent.tb_bg_color_sel_hover:
+			parent.tb_hover = True
+			label = parent.tb_bg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_bg_color_sel_hover = color
+
+		# QSpinBox
+		if not parent.sb_bg_color_sel_hover:
+			parent.sb_hover = True
+			label = parent.sb_bg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.sb_bg_color_sel_hover = color
+
+		# QLabel
+		if not parent.lb_bg_color_sel_hover:
+			parent.lb_hover = True
+			label = parent.lb_bg_color_hover.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.lb_bg_color_sel_hover = color
+
+
+	# Foreground Color Checked
+	if parent.all_fg_color_sel_checked:
+		color = parent.all_fg_color_sel_checked
+
+		# QPushButton
+		if not parent.pb_fg_color_sel_checked:
+			parent.pb_checked = True
+			label = parent.pb_fg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_fg_color_sel_checked = color
+
+		# QCheckBox
+		if not parent.cb_fg_color_sel_checked:
+			parent.cb_checked = True
+			label = parent.cb_fg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_fg_color_sel_checked = color
+
+		# QRadioButton
+		if not parent.rb_fg_color_sel_checked:
+			parent.rb_checked = True
+			label = parent.rb_fg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_fg_color_sel_checked = color
+
+		# QToolButton
+		if not parent.tb_fg_color_sel_checked:
+			parent.tb_checked = True
+			label = parent.tb_fg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_fg_color_sel_checked = color
+
+	# Background Color Checked
+	if parent.all_bg_color_sel_checked:
+		color = parent.all_bg_color_sel_checked
+
+		# QPushButton
+		if not parent.pb_bg_color_sel_checked:
+			parent.pb_checked = True
+			label = parent.pb_bg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_bg_color_sel_checked = color
+
+		# QCheckBox
+		if not parent.cb_bg_color_sel_checked:
+			parent.cb_checked = True
+			label = parent.cb_bg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_bg_color_sel_checked = color
+
+		# QRadioButton
+		if not parent.rb_bg_color_sel_checked:
+			parent.rb_checked = True
+			label = parent.rb_bg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_bg_color_sel_checked = color
+
+		# QToolButton
+		if not parent.tb_bg_color_sel_checked:
+			parent.tb_checked = True
+			label = parent.tb_bg_color_checked.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_bg_color_sel_checked = color
 
 	# Foreground Color Disabled
 	if parent.all_fg_color_sel_disabled:
 		color = parent.all_fg_color_sel_disabled
 
 		# QPushButton
-		parent.pb_disabled = True
-		label = parent.pb_fg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.pb_fg_color_sel_disabled = color
+		if not parent.pb_fg_color_sel_disabled:
+			parent.pb_disabled = True
+			label = parent.pb_fg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_fg_color_sel_disabled = color
 
 		# QCheckBox
-		parent.cb_disabled = True
-		label = parent.cb_fg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.cb_fg_color_sel_disabled = color
+		if not parent.cb_fg_color_sel_disabled:
+			parent.cb_disabled = True
+			label = parent.cb_fg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_fg_color_sel_disabled = color
 
 		# QRadioButton
-		parent.rb_disabled = True
-		label = parent.rb_fg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.rb_fg_color_sel_disabled = color
+		if not parent.rb_fg_color_sel_disabled:
+			parent.rb_disabled = True
+			label = parent.rb_fg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_fg_color_sel_disabled = color
 
 		# QToolButton
-		parent.tb_disabled = True
-		label = parent.tb_fg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.tb_fg_color_sel_disabled = color
+		if not parent.tb_fg_color_sel_disabled:
+			parent.tb_disabled = True
+			label = parent.tb_fg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_fg_color_sel_disabled = color
 
 		# QSpinBox
-		parent.sb_disabled = True
-		label = parent.sb_fg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.sb_fg_color_sel_disabled = color
+		if not parent.sb_fg_color_sel_disabled:
+			parent.sb_disabled = True
+			label = parent.sb_fg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.sb_fg_color_sel_disabled = color
 
 		# QLabel
-		parent.lb_disabled = True
-		label = parent.lb_fg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.lb_fg_color_sel_disabled = color
+		if not parent.lb_fg_color_sel_disabled:
+			parent.lb_disabled = True
+			label = parent.lb_fg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.lb_fg_color_sel_disabled = color
 
 	# Background Color Disabled
 	if parent.all_bg_color_sel_disabled:
 		color = parent.all_bg_color_sel_disabled
 
 		# QPushButton
-		parent.pb_disabled = True
-		label = parent.pb_bg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.pb_bg_color_sel_disabled = color
+		if not parent.pb_bg_color_sel_disabled:
+			parent.pb_disabled = True
+			label = parent.pb_bg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.pb_bg_color_sel_disabled = color
 
 		# QCheckBox
-		parent.cb_disabled = True
-		label = parent.cb_bg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.cb_bg_color_sel_disabled = color
+		if not parent.cb_bg_color_sel_disabled:
+			parent.cb_disabled = True
+			label = parent.cb_bg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.cb_bg_color_sel_disabled = color
 
 		# QRadioButton
-		parent.rb_disabled = True
-		label = parent.rb_bg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.rb_bg_color_sel_disabled = color
+		if not parent.rb_bg_color_sel_disabled:
+			parent.rb_disabled = True
+			label = parent.rb_bg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.rb_bg_color_sel_disabled = color
 
 		# QToolButton
-		parent.tb_disabled = True
-		label = parent.tb_bg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.tb_bg_color_sel_disabled = color
+		if not parent.tb_bg_color_sel_disabled:
+			parent.tb_disabled = True
+			label = parent.tb_bg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.tb_bg_color_sel_disabled = color
 
 		# QSpinBox
-		parent.sb_disabled = True
-		label = parent.sb_bg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.sb_bg_color_sel_disabled = color
+		if not parent.sb_bg_color_sel_disabled:
+			parent.sb_disabled = True
+			label = parent.sb_bg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.sb_bg_color_sel_disabled = color
 
 		# QLabel
-		parent.lb_disabled = True
-		label = parent.lb_bg_color_disabled.property('label')
-		getattr(parent, label).setStyleSheet(f'background-color: {color};')
-		parent.lb_bg_color_sel_disabled = color
+		if not parent.lb_bg_color_sel_disabled:
+			parent.lb_disabled = True
+			label = parent.lb_bg_color_disabled.property('label')
+			getattr(parent, label).setStyleSheet(f'background-color: {color};')
+			parent.lb_bg_color_sel_disabled = color
 
 
 
