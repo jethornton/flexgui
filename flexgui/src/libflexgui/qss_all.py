2debe18a54a98a6b80fc44999c70b31304383a48
John Thornton <dev@gnipsel.com>
2025-01-02 06:27:27 -0600

    add all disabled
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/qss_all.py
+++ flexgui/src/libflexgui/qss_all.py
@@ -20,10 +20,16 @@ def startup(parent):
 	parent.all_fg_color_sel_normal = False
 	parent.all_bg_color_sel_normal = False
 
+	parent.all_fg_color_disabled.clicked.connect(parent.color_dialog)
+	parent.all_bg_color_disabled.clicked.connect(parent.color_dialog)
+
+	parent.all_fg_color_sel_disabled = False
+	parent.all_bg_color_sel_disabled = False
+
 def all_create_stylesheet(parent):
 	style = False
 
-	# Foreground Color
+	# Foreground Color Normal
 	if parent.all_fg_color_sel_normal:
 		color = parent.all_fg_color_sel_normal
 
@@ -63,7 +69,7 @@ def all_create_stylesheet(parent):
 		getattr(parent, label).setStyleSheet(f'background-color: {color};')
 		parent.lb_fg_color_sel_normal = color
 
-	# Background Color
+	# Background Color Normal
 	if parent.all_bg_color_sel_normal:
 		color = parent.all_bg_color_sel_normal
 
@@ -102,6 +108,91 @@ def all_create_stylesheet(parent):
 		label = parent.lb_bg_color_normal.property('label')
 		getattr(parent, label).setStyleSheet(f'background-color: {color};')
 		parent.lb_bg_color_sel_normal = color
+
+	# Foreground Color Disabled
+	if parent.all_fg_color_sel_disabled:
+		color = parent.all_fg_color_sel_disabled
+
+		# QPushButton
+		parent.pb_disabled = True
+		label = parent.pb_fg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.pb_fg_color_sel_disabled = color
+
+		# QCheckBox
+		parent.cb_disabled = True
+		label = parent.cb_fg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.cb_fg_color_sel_disabled = color
+
+		# QRadioButton
+		parent.rb_disabled = True
+		label = parent.rb_fg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.rb_fg_color_sel_disabled = color
+
+		# QToolButton
+		parent.tb_disabled = True
+		label = parent.tb_fg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.tb_fg_color_sel_disabled = color
+
+		# QSpinBox
+		parent.sb_disabled = True
+		label = parent.sb_fg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.sb_fg_color_sel_disabled = color
+
+		# QLabel
+		parent.lb_disabled = True
+		label = parent.lb_fg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.lb_fg_color_sel_disabled = color
+
+	# Background Color Disabled
+	if parent.all_bg_color_sel_disabled:
+		color = parent.all_bg_color_sel_disabled
+
+		# QPushButton
+		parent.pb_disabled = True
+		label = parent.pb_bg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.pb_bg_color_sel_disabled = color
+
+		# QCheckBox
+		parent.cb_disabled = True
+		label = parent.cb_bg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.cb_bg_color_sel_disabled = color
+
+		# QRadioButton
+		parent.rb_disabled = True
+		label = parent.rb_bg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.rb_bg_color_sel_disabled = color
+
+		# QToolButton
+		parent.tb_disabled = True
+		label = parent.tb_bg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.tb_bg_color_sel_disabled = color
+
+		# QSpinBox
+		parent.sb_disabled = True
+		label = parent.sb_bg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.sb_bg_color_sel_disabled = color
+
+		# QLabel
+		parent.lb_disabled = True
+		label = parent.lb_bg_color_disabled.property('label')
+		getattr(parent, label).setStyleSheet(f'background-color: {color};')
+		parent.lb_bg_color_sel_disabled = color
+
+
+
+
+
 
 	qss_pushbutton.create_stylesheet(parent)
 	qss_checkbox.create_stylesheet(parent)
