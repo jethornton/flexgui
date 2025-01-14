41618ec07f77716262eb51171c6b0712dd55e258
John Thornton <dev@gnipsel.com>
2025-01-14 06:29:54 -0600

    fix var name
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/qss_all.py
+++ flexgui/src/libflexgui/qss_all.py
@@ -69,7 +69,7 @@ def all_create_stylesheet(parent):
 			parent.rb_fg_color_sel_normal = color
 
 		# QToolBar
-		if not parent.toolbar_bg_color:
+		if not parent.toolbar_bg_sel_color:
 			parent.tbar_normal = True
 			label = parent.tbar_bg_color_normal.property('label')
 			getattr(parent, label).setStyleSheet(f'background-color: {color};')
