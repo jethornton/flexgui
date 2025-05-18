8159aaae87f92ee5180b185e274a6157665deb3e
John Thornton <dev@gnipsel.com>
2025-05-18 07:55:14 -0500

    remove depreciated property check
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/startup.py
+++ flexgui/src/libflexgui/startup.py
@@ -1435,12 +1435,7 @@ def setup_set_var(parent):
 	parent.set_var = {}
 	for child in parent.findChildren(QDoubleSpinBox):
 		prop = child.property('function')
-		if prop == 'get_var':
-			msg = ('The property get_var is deprecated\n'
-			'and will be removed soon.\n'
-			'Use the property set_var for Setting Parameters')
-			dialogs.warn_msg_ok(parent, msg, 'Property Deprecated')
-		if prop == 'set_var' or prop == 'get_var': # FIXME someday remove get_var
+		if prop == 'set_var':
 			var = child.property('variable')
 			found = False
 			if var is not None:
