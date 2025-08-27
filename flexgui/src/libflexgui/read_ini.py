cca3dc7c3d314408888874e5b0b1b20c45255de2
John Thornton <dev@gnipsel.com>
2025-08-27 06:36:04 -0500

    check ini for keyboard jogging
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/read_ini.py
+++ flexgui/src/libflexgui/read_ini.py
@@ -7,20 +7,42 @@ from libflexgui import dialogs
 from libflexgui import utilities
 
 def read(parent):
+
+	# FIXME remove these tests 6-1-25
+	if parent.inifile.find('FLEX_COLORS', 'ESTOP_OPEN'):
+		msg = ('The colors for E Stop and Power buttons\n'
+		'has been moved to the [FLEXGUI] section\n'
+		'of the ini file.\n'
+		'See the INI Settings section of the\n'
+		'documents for more information.')
+		dialogs.warn_msg_ok(parent, msg, 'Update the INI file')
+
+	if parent.inifile.find('FLEX', 'PLOT_BACKGROUND_COLOR'):
+		msg = ('The [FLEX] section has been changed to [FLEXGUI]\n'
+		'The key PLOT_BACKGROUND_COLOR needs to be in the [FLEXGUI] section\n'
+		'Check the Plotter section of the Documents for correct INI entries.')
+		dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
+
+	if parent.inifile.find('FLEX', 'TOUCH_FILE_WIDTH'):
+		msg = ('The [FLEX] section has been changed to [FLEXGUI]\n'
+		'The key TOUCH_FILE_WIDTH needs to be in the [FLEXGUI] section\n'
+		'Check the Plotter section of the Documents for correct INI entries.')
+		dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
+
+	if parent.inifile.find('FLEX', 'MANUAL_TOOL_CHANGE'):
+		msg = ('The [FLEX] section has been changed to [FLEXGUI]\n'
+		'The key MANUAL_TOOL_CHANGE needs to be in the [FLEXGUI] section\n'
+		'Check the Tools section of the Documents for correct INI entries.')
+		dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
+
+	# ***** [EMC] Section *****
 	machine_name = parent.inifile.find('EMC', 'MACHINE') or False
 	if machine_name:
 		parent.settings = QSettings('Flex', machine_name)
 	else:
 		parent.settings = QSettings('Flex', 'unknown')
 
-	units = parent.inifile.find('TRAJ', 'LINEAR_UNITS') or False # mm or inch
-	if units.lower() == 'inch':
-		parent.default_precision = 4
-	elif units.lower() == 'mm':
-		parent.default_precision = 3
-	else:
-		parent.default_precision = 4
-
+	# ***** [DISPLAY] Section *****
 	# get file extensions
 	parent.extensions = ['.ngc'] # used by the touch file selector
 	extensions = parent.inifile.find('DISPLAY', 'EXTENSIONS') or False
@@ -33,15 +55,50 @@ def read(parent):
 	else:
 		parent.ext_filter = 'G code Files (*.ngc *.NGC);;All Files (*)'
 
-	# FIXME remove these tests 6-1-25
-	if parent.inifile.find('FLEX_COLORS', 'ESTOP_OPEN'):
-		msg = ('The colors for E Stop and Power buttons\n'
-		'has been moved to the [FLEXGUI] section\n'
-		'of the ini file.\n'
-		'See the INI Settings section of the\n'
-		'documents for more information.')
-		dialogs.warn_msg_ok(parent, msg, 'Update the INI file')
+	# set the nc code directory to some valid directory
+	directory = parent.inifile.find('DISPLAY', 'PROGRAM_PREFIX') or False
+	ini_dir = False
+	if directory: # expand directory if needed
+		ini_dir = True
+		if directory.startswith('./'): # in this directory
+			directory = os.path.join(parent.config_path, directory[2:])
+		elif directory.startswith('../'): # up one directory
+			directory = os.path.join(os.path.dirname(parent.config_path), directory[3:])
+		elif directory.startswith('~'): # users home directory
+			directory = os.path.expanduser(directory)
 
+	if os.path.isdir(directory):
+		parent.nc_code_dir = directory
+	else: # try and find a directory
+		if os.path.isdir(os.path.expanduser('~/linuxcnc/nc_files')):
+			parent.nc_code_dir = os.path.expanduser('~/linuxcnc/nc_files')
+		else:
+			parent.nc_code_dir = os.path.expanduser('~/')
+		if ini_dir: # a nc code directory was in the ini file but is not valid
+			msg = (f'The path {directory}\n'
+				'does not exist. Check the\n'
+				'PROGRAM_PREFIX key in the\n'
+				'[DISPLAY] section of the\n'
+				'INI file for a valid path.\n'
+				f'{parent.nc_code_dir} will be used.')
+			dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
+
+	parent.editor = parent.inifile.find('DISPLAY', 'EDITOR') or False
+	parent.tool_editor = parent.inifile.find('DISPLAY', 'TOOL_EDITOR') or False
+	if parent.inifile.find('DISPLAY', 'LATHE') is not None:
+		parent.default_view = 'y'
+	elif parent.inifile.find('DISPLAY', 'VIEW') is not None:
+		parent.default_view = parent.inifile.find('DISPLAY', 'VIEW')
+	else:
+		parent.default_view = 'p'
+
+	# ***** [EMCIO] Section *****
+	parent.tool_table = parent.inifile.find('EMCIO', 'TOOL_TABLE') or False
+
+	# ***** [RS274NGC] Section *****
+	parent.var_file = parent.inifile.find('RS274NGC', 'PARAMETER_FILE') or False
+
+	# ***** [FLEXGUI] Section *****
 	# check for LED defaults in the ini file
 	parent.led_diameter = parent.inifile.find('FLEXGUI', 'LED_DIAMETER')
 	if parent.led_diameter is None:
@@ -109,87 +166,33 @@ def read(parent):
 		if color:
 			parent.probe_enable_off_color = f'background-color: {color};'
 
-	# ***** [TRAJ] Section *****
-	units = parent.inifile.find('TRAJ', 'LINEAR_UNITS') or False
-	if units == 'inch':
-		parent.units = 'in'
-	else:
-		parent.units = 'mm'
-
-	''' MAX_LINEAR_VELOCITY = 5.0 - The maximum velocity for any axis or coordinated
-	move, in machine units per second. The value shown equals 300 units per minute. '''
-	parent.max_linear_vel = parent.inifile.find('TRAJ', 'MAX_LINEAR_VELOCITY') or False
-
-	# set the nc code directory to some valid directory
-	directory = parent.inifile.find('DISPLAY', 'PROGRAM_PREFIX') or False
-	ini_dir = False
-	if directory: # expand directory if needed
-		ini_dir = True
-		if directory.startswith('./'): # in this directory
-			directory = os.path.join(parent.config_path, directory[2:])
-		elif directory.startswith('../'): # up one directory
-			directory = os.path.join(os.path.dirname(parent.config_path), directory[3:])
-		elif directory.startswith('~'): # users home directory
-			directory = os.path.expanduser(directory)
-
-	if os.path.isdir(directory):
-		parent.nc_code_dir = directory
-	else: # try and find a directory
-		if os.path.isdir(os.path.expanduser('~/linuxcnc/nc_files')):
-			parent.nc_code_dir = os.path.expanduser('~/linuxcnc/nc_files')
-		else:
-			parent.nc_code_dir = os.path.expanduser('~/')
-		if ini_dir: # a nc code directory was in the ini file but is not valid
-			msg = (f'The path {directory}\n'
-				'does not exist. Check the\n'
-				'PROGRAM_PREFIX key in the\n'
-				'[DISPLAY] section of the\n'
-				'INI file for a valid path.\n'
-				f'{parent.nc_code_dir} will be used.')
-			dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
-
-	parent.editor = parent.inifile.find('DISPLAY', 'EDITOR') or False
-	parent.tool_editor = parent.inifile.find('DISPLAY', 'TOOL_EDITOR') or False
-	if parent.inifile.find('DISPLAY', 'LATHE') is not None:
-		parent.default_view = 'y'
-	elif parent.inifile.find('DISPLAY', 'VIEW') is not None:
-		parent.default_view = parent.inifile.find('DISPLAY', 'VIEW')
-	else:
-		parent.default_view = 'p'
-
-	parent.tool_table = parent.inifile.find('EMCIO', 'TOOL_TABLE') or False
-	parent.var_file = parent.inifile.find('RS274NGC', 'PARAMETER_FILE') or False
-
-	if parent.inifile.find('FLEX', 'PLOT_BACKGROUND_COLOR'):
-		msg = ('The [FLEX] section has been changed to [FLEXGUI]\n'
-		'The key PLOT_BACKGROUND_COLOR needs to be in the [FLEXGUI] section\n'
-		'Check the Plotter section of the Documents for correct INI entries.')
-		dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
-
 	parent.plot_background_color = parent.inifile.find('FLEXGUI', 'PLOT_BACKGROUND_COLOR') or False
-	#print(type(background_color))
-	#print(background_color)
 	if parent.plot_background_color:
 		parent.plot_background_color = tuple(map(float, parent.plot_background_color.split(',')))
 
-	if parent.inifile.find('FLEX', 'TOUCH_FILE_WIDTH'):
-		msg = ('The [FLEX] section has been changed to [FLEXGUI]\n'
-		'The key TOUCH_FILE_WIDTH needs to be in the [FLEXGUI] section\n'
-		'Check the Plotter section of the Documents for correct INI entries.')
-		dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
+	parent.manual_tool_change = parent.inifile.find('FLEXGUI', 'MANUAL_TOOL_CHANGE') or False
 
-	parent.touch_file_width = parent.inifile.find('FLEX', 'TOUCH_FILE_WIDTH') or False
+	parent.touch_file_width = parent.inifile.find('FLEXGUI', 'TOUCH_FILE_WIDTH') or False
 	if parent.touch_file_width in ['True', 'true', '1']:
 		parent.touch_file_width = True
 	else:
 		parent.touch_file_width = False
 
-	if parent.inifile.find('FLEX', 'MANUAL_TOOL_CHANGE'):
-		msg = ('The [FLEX] section has been changed to [FLEXGUI]\n'
-		'The key MANUAL_TOOL_CHANGE needs to be in the [FLEXGUI] section\n'
-		'Check the Tools section of the Documents for correct INI entries.')
-		dialogs.warn_msg_ok(parent, msg, 'Configuration Error')
+	parent.enable_kb_jogging = parent.inifile.find('FLEXGUI', 'KEYBOARD_JOG') or False
 
-	parent.manual_tool_change = parent.inifile.find('FLEXGUI', 'MANUAL_TOOL_CHANGE') or False
+	# ***** [TRAJ] Section *****
+	units = parent.inifile.find('TRAJ', 'LINEAR_UNITS') or False # mm or inch
+	if units.lower() == 'inch':
+		parent.units = 'in'
+		parent.default_precision = 4
+	elif units.lower() == 'mm':
+		parent.units = 'mm'
+		parent.default_precision = 3
+	else:
+		parent.default_precision = 4
+
+	''' MAX_LINEAR_VELOCITY = 5.0 - The maximum velocity for any axis or coordinated
+	move, in machine units per second. The value shown equals 300 units per minute. '''
+	parent.max_linear_vel = parent.inifile.find('TRAJ', 'MAX_LINEAR_VELOCITY') or False
 
 
