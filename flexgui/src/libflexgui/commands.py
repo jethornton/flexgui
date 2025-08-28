9dd4de21a61ab43117176c20ab9fd840ceb07f47
John Thornton <dev@gnipsel.com>
2025-08-28 11:22:39 -0500

    move keyboard jog to commands.py
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/commands.py
+++ flexgui/src/libflexgui/commands.py
@@ -198,6 +198,33 @@ def jog_selected(parent):
 				parent.override_limits_cb.setChecked(False)
 				parent.override_limits_cb.setEnabled(False)
 
+def keyboard_jog(parent, action, axis, direction=None):
+	vel = parent.jog_vel_sl.value() / 60
+	if direction == 'neg':
+		vel = -vel
+	if parent.status.motion_mode == emc.TRAJ_MODE_FREE:
+		teleop_mode = 0
+		joint_jog = True
+	else: # all axes are homed
+		teleop_mode = 1
+		joint_jog = False
+
+	if parent.status.task_mode == emc.MODE_MANUAL and action:
+
+		# set teleop mode
+		parent.command.teleop_enable(teleop_mode)
+		parent.command.wait_complete()
+
+		# set distance if jog_modes_cb.currenData() is not false
+		distance = parent.jog_modes_cb.currentData()
+		if distance:
+			parent.command.jog(emc.JOG_INCREMENT, joint_jog, axis, vel, distance)
+		else:
+			parent.command.jog(emc.JOG_CONTINUOUS, joint_jog, axis, vel)
+	else:
+		parent.command.jog(emc.JOG_STOP, joint_jog, axis)
+
+
 def mdi_button(parent, button):
 	mdi_command = button.property('command')
 	if mdi_command:
