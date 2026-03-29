e6febbda3f71849aac74d87ffda2f159ede839a2
John Thornton <dev@gnipsel.com>
2026-03-29 09:46:08 -0500

    add navigation buttons to keyboard popup
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/keyboard_pad.py
+++ flexgui/src/libflexgui/keyboard_pad.py
@@ -9,11 +9,14 @@ class keyboard_pad(QDialog):
 		super().__init__()
 
 		loadUi(os.path.join(lib_path, 'keyboard.ui'), self)
+		self.keyboard_le.setReadOnly(False)
 		self.save_pb.clicked.connect(self.accept)
 		self.cancel_pb.clicked.connect(self.reject)
 		self.clear_pb.clicked.connect(self.clear)
 		self.space_pb.clicked.connect(self.space)
 		self.backspace_pb.clicked.connect(self.backspace)
+		self.left_arrow_pb.clicked.connect(self.left_arrow)
+		self.right_arrow_pb.clicked.connect(self.right_arrow)
 		self.gcode_pb.clicked.connect(partial(self.change_page, 0))
 		self.capital_letters_pb.clicked.connect(partial(self.change_page, 1))
 		self.lower_letters_pb.clicked.connect(partial(self.change_page, 2))
@@ -27,27 +30,36 @@ class keyboard_pad(QDialog):
 		self.exit_pos = None
 
 	def post(self):
-		txt = self.keyboard_lb.text()
-		self.keyboard_lb.setText(f'{txt}{self.sender().text()}')
+		self.keyboard_le.insert(self.sender().text())
 
 	def clear(self):
-		self.keyboard_lb.clear()
+		self.keyboard_le.clear()
 
 	def space(self):
-		txt = self.keyboard_lb.text()
-		self.keyboard_lb.setText(f'{txt} ')
+		self.keyboard_le.insert(' ')
 
 	def backspace(self):
-		txt = self.keyboard_lb.text()
-		if len(txt) > 0:
-			self.keyboard_lb.setText(txt[:-1])
+		self.keyboard_le.backspace()
+		self.keyboard_le.setFocus()
+
+	def left_arrow(self):
+		# Move one step backward from the current position
+		current_pos = self.keyboard_le.cursorPosition()
+		self.keyboard_le.setCursorPosition(max(0, current_pos - 1)) # Ensure position is not negative
+		self.keyboard_le.setFocus()
+
+	def right_arrow(self):
+		# Move one step forward from the current position
+		current_pos = self.keyboard_le.cursorPosition()
+		self.keyboard_le.setCursorPosition(current_pos + 1)
+		self.keyboard_le.setFocus()
 
 	def change_page(self, index):
 		self.keyboard_sw.setCurrentIndex(index)
 
 	def retval(self):
 		try:
-			return(self.keyboard_lb.text())
+			return(self.keyboard_le.text())
 		except:
 			return False
 
