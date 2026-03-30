67423fe8a134829fc4b5cf4d36a0225118673eb7
John Thornton <dev@gnipsel.com>
2026-03-30 06:08:16 -0500

    add navigation to gcode popup
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/gcode_pad.py
+++ flexgui/src/libflexgui/gcode_pad.py
@@ -12,6 +12,8 @@ class gcode_pad(QDialog):
 		self.cancel_pb.clicked.connect(self.reject)
 		self.clear_pb.clicked.connect(self.clear)
 		self.backspace_pb.clicked.connect(self.backspace)
+		self.left_arrow_pb.clicked.connect(self.left_arrow)
+		self.right_arrow_pb.clicked.connect(self.right_arrow)
 		self.space_pb.clicked.connect(self.space)
 
 		for item in self.findChildren(QPushButton):
@@ -32,20 +34,32 @@ class gcode_pad(QDialog):
 		self.letters_sw.setCurrentIndex(self.letters_sw.currentIndex() - 1)
 
 	def post(self):
-		txt = self.gcode_le.text()
-		self.gcode_le.setText(f'{txt}{self.sender().text()}')
+		self.gcode_le.insert(self.sender().text())
+		self.gcode_le.setFocus()
 
 	def clear(self):
 		self.gcode_le.clear()
+		self.gcode_le.setFocus()
 
 	def backspace(self):
-		txt = self.gcode_le.text()
-		if len(txt) > 0:
-			self.gcode_le.setText(txt[:-1])
+		self.gcode_le.backspace()
+		self.gcode_le.setFocus()
+
+	def left_arrow(self):
+		# Move one step backward from the current position
+		current_pos = self.gcode_le.cursorPosition()
+		self.gcode_le.setCursorPosition(max(0, current_pos - 1)) # Ensure position is not negative
+		self.gcode_le.setFocus()
+
+	def right_arrow(self):
+		# Move one step forward from the current position
+		current_pos = self.gcode_le.cursorPosition()
+		self.gcode_le.setCursorPosition(current_pos + 1)
+		self.gcode_le.setFocus()
 
 	def space(self):
-		txt = self.gcode_le.text()
-		self.gcode_le.setText(f'{txt} ')
+		self.gcode_le.insert(' ')
+		self.gcode_le.setFocus()
 
 	def retval(self):
 		try:
