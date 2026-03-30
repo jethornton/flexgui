4f25d7e8a38eabd48b331dec4d2cf9f83546b444
John Thornton <dev@gnipsel.com>
2026-03-30 06:20:24 -0500

    add navigation to number popup
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/numpad.py
+++ flexgui/src/libflexgui/numpad.py
@@ -12,6 +12,8 @@ class number_pad(QDialog):
 		self.cancel_pb.clicked.connect(self.reject)
 		self.clear_pb.clicked.connect(self.clear)
 		self.minus.clicked.connect(self.dash)
+		self.left_arrow_pb.clicked.connect(self.left_arrow)
+		self.right_arrow_pb.clicked.connect(self.right_arrow)
 		self.backspace_pb.clicked.connect(self.backspace)
 		for item in self.findChildren(QPushButton):
 			if item.objectName().startswith('key_'):
@@ -21,20 +23,33 @@ class number_pad(QDialog):
 		self.exit_pos = None
 
 	def post(self):
-		txt = self.numbers_le.text()
-		self.numbers_le.setText(f'{txt}{self.sender().text()}')
+		self.numbers_le.insert(self.sender().text())
+		self.numbers_le.setFocus()
 
 	def clear(self):
 		self.numbers_le.clear()
+		self.numbers_le.setFocus()
 
 	def dash(self):
 		txt = self.numbers_le.text()
 		self.numbers_le.setText(f'-{txt}')
+		self.numbers_le.setFocus()
+
+	def left_arrow(self):
+		# Move one step backward from the current position
+		current_pos = self.numbers_le.cursorPosition()
+		self.numbers_le.setCursorPosition(max(0, current_pos - 1)) # Ensure position is not negative
+		self.numbers_le.setFocus()
+
+	def right_arrow(self):
+		# Move one step forward from the current position
+		current_pos = self.numbers_le.cursorPosition()
+		self.numbers_le.setCursorPosition(current_pos + 1)
+		self.numbers_le.setFocus()
 
 	def backspace(self):
-		txt = self.numbers_le.text()
-		if len(txt) > 0:
-			self.numbers_le.setText(txt[:-1])
+		self.numbers_le.backspace()
+		self.numbers_le.setFocus()
 
 	def retval(self):
 		try:
