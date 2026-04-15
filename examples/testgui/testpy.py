982dc57eeec1829b30a65c4c1295167fe133aae2
John Thornton <dev@gnipsel.com>
2026-04-15 07:47:14 -0500

    update example
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- examples/testgui/testpy.py
+++ examples/testgui/testpy.py
@@ -1,8 +1,9 @@
 from functools import partial
 
 def startup(parent):
-	# connect a pushbutton without passing parent
-	parent.external_tc_pb.clicked.connect(partial(external_tc, parent))
+	parent.external_tc_pb.toggled.connect(partial(external_tc, parent))
 
 def external_tc(parent):
-	parent.tool_change_pb.click()
+	if not parent.sender().isChecked():
+		parent.tool_change_pb.click()
+
