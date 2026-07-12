ecc943b863a8020d7d5d82ce4c30402f1ba076b5
John Thornton <dev@gnipsel.com>
2026-07-12 06:31:19 -0500

    remove test examples
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- examples/testgui/Errors/post.py
+++
@@ -1,4 +0,0 @@
-
-
-def startup(parent):
-	print('post startup')
--- examples/testgui/testpy.py
+++
@@ -1,9 +0,0 @@
-from functools import partial
-
-def startup(parent):
-	parent.external_tc_pb.toggled.connect(partial(external_tc, parent))
-
-def external_tc(parent):
-	if not parent.sender().isChecked():
-		parent.tool_change_pb.click()
-
